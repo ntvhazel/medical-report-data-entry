ec62f37ad62d6abeadcc07c2acb788549772144a
ntvhazel <vtthuan456@gmail.com>
2023-08-09 00:40:18 +0700

    new

--- app.py
+++ app.py
@@ -112,17 +112,17 @@ def perspective_transform(image, pts):
     return warped
 
 ready = False
-type = st.secrets["a"]
-project_id = st.secrets["b"]
-private_key = st.secrets["c"]
-private_key_id = st.secrets["d"]
-client_email = st.secrets["e"]
-client_id = st.secrets["f"]
-auth_uri = st.secrets["g"]
-token_uri = st.secrets["h"]
-auth_provider_x509_cert_url = st.secrets["i"]
-client_x509_cert_url = st.secrets["j"]
-universe_domain = st.secrets["k"]
+type = st.secrets["type"]
+project_id = st.secrets["project_id"]
+private_key = st.secrets["private_key"]
+private_key_id = st.secrets["private_key_id"]
+client_email = st.secrets["client_email"]
+client_id = st.secrets["client_id"]
+auth_uri = st.secrets["auth_uri"]
+token_uri = st.secrets["token_uri"]
+auth_provider_x509_cert_url = st.secrets["auth_provider_x509_cert_url"]
+client_x509_cert_url = st.secrets["client_x509_cert_url"]
+universe_domain = st.secrets["universe_domain"]
 
 api_key = {
     "type": type,
