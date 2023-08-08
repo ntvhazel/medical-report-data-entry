79b2262b310e44785e8857982c9380fe7d605857
ntvhazel <vtthuan456@gmail.com>
2023-08-09 00:12:11 +0700

    new

--- app.py
+++ app.py
@@ -113,17 +113,17 @@ def perspective_transform(image, pts):
 
 ready = False
 api_key = {
-    "type": st.secrets["type"],
-    "project_id": st.secrets["project_id"],
-    "private_key_id":st.secrets["private_key_id"],
-    "private_key":st.secrets["private_key"],
-    "client_email":st.secrets["client_email"],
-    "client_id":st.secrets["client_id"],
-    "auth_uri":st.secrets["auth_uri"],
-    "token_uri":st.secrets["token_uri"],
-    "auth_provider_x509_cert_url":st.secrets["auth_provider_x509_cert_url"],
-    "client_x509_cert_url":st.secrets["client_x509_cert_url"],
-    "universe_domain":st.secrets["universe_domain"]
+    "type": st.secrets['gcp_service_account']["type"],
+    "project_id": st.secrets['gcp_service_account']["project_id"],
+    "private_key_id":st.secrets['gcp_service_account']["private_key_id"],
+    "private_key":st.secrets['gcp_service_account']["private_key"],
+    "client_email":st.secrets['gcp_service_account']["client_email"],
+    "client_id":st.secrets['gcp_service_account']["client_id"],
+    "auth_uri":st.secrets['gcp_service_account']["auth_uri"],
+    "token_uri":st.secrets['gcp_service_account']["token_uri"],
+    "auth_provider_x509_cert_url":st.secrets['gcp_service_account']["auth_provider_x509_cert_url"],
+    "client_x509_cert_url":st.secrets['gcp_service_account']["client_x509_cert_url"],
+    "universe_domain":st.secrets['gcp_service_account']["universe_domain"]
 }
 # ---------- OPERATIONS ----------
 option = st.radio(
